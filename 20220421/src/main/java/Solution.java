5db6ab11c10235cb6f88c9eeb934b2b34fde60f3
pangnem <sprnd645@gmail.com>
2022-04-21 20:09:06 +0900

    20220421 약수 구하는 메서드 분리

--- 20220421/src/main/java/Solution.java
+++ 20220421/src/main/java/Solution.java
@@ -2,16 +2,10 @@ public class Solution {
     public int solution(int left, int right) {
         int result = 0;
         for (int i = left; i <= right; i++) {
-            int divisorCount = 0;
-            for (int j = 1; j <= i / 2; j++) {
-                if (i % j == 0) {
-                    divisorCount++;
-                }
-            }
-            divisorCount++;
+            int divisorCount = getDivisorCount(i);
 
             if (divisorCount % 2 == 0) {
-               result += i;
+                result += i;
             }
 
             if (divisorCount % 2 != 0) {
@@ -20,5 +14,16 @@ public class Solution {
         }
 
         return result;
+    }
+
+    private int getDivisorCount(int number) {
+        int divisorCount = 0;
+        for (int j = 1; j <= number / 2; j++) {
+            if (number % j == 0) {
+                divisorCount++;
+            }
+        }
+
+        return ++divisorCount;
     }
 }
