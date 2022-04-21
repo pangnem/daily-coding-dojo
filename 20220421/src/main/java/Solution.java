de52561b0b5d547d3fb4f1d3be033ee8ccdc45c7
pangnem <sprnd645@gmail.com>
2022-04-21 20:12:56 +0900

    20220421 결과 구하는 메서드 분리

--- 20220421/src/main/java/Solution.java
+++ 20220421/src/main/java/Solution.java
@@ -1,29 +1,32 @@
+import java.util.stream.IntStream;
+
 public class Solution {
     public int solution(int left, int right) {
         int result = 0;
         for (int i = left; i <= right; i++) {
             int divisorCount = getDivisorCount(i);
 
-            if (divisorCount % 2 == 0) {
-                result += i;
-            }
+            result = getResult(result, i, divisorCount);
+        }
 
-            if (divisorCount % 2 != 0) {
-                result -= i;
-            }
+        return result;
+    }
+
+    private int getResult(int result, int i, int divisorCount) {
+        if (divisorCount % 2 == 0) {
+            result += i;
+        }
+
+        if (divisorCount % 2 != 0) {
+            result -= i;
         }
 
         return result;
     }
 
     private int getDivisorCount(int number) {
-        int divisorCount = 0;
-        for (int j = 1; j <= number / 2; j++) {
-            if (number % j == 0) {
-                divisorCount++;
-            }
-        }
-
-        return ++divisorCount;
+        return (int) IntStream.rangeClosed(1, number)
+                .filter(it -> number % it == 0)
+                .count();
     }
 }
