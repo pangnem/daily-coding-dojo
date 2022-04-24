da0280a99c24f4732487c50569d49418bde14e73
pangnem <sprnd645@gmail.com>
2022-04-24 23:01:47 +0900

    20220424 다른사람의 풀이

--- 20220424/README.md
+++ 20220424/README.md
@@ -1,5 +1,9 @@
 # 20220424
 Programmers 직사각형 별찍기 
 
+### 다른사람의 풀이
+IntStream으로 순회하는 것은 같은데, StringBuilder를 사용한 부분이 신선했다.
+- (별 한줄을 stringBuilder에 저장하고 횟수만큼 반복해서 출력)
+
 ### 문제 링크
 - https://programmers.co.kr/learn/courses/30/lessons/12969?language=java
--- 20220424/src/main/java/Solution.java
+++ 20220424/src/main/java/Solution.java
@@ -3,6 +3,11 @@ import java.util.stream.IntStream;
 
 class Solution {
     public static void main(String[] args) {
+//        solution1();
+        solution2();
+    }
+
+    private static void solution1() {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
@@ -14,5 +19,16 @@ class Solution {
 
                     System.out.println();
                 });
+    }
+
+    // 다른사람의 풀이
+    private static void solution2() {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        StringBuilder stringBuilder = new StringBuilder();
+        IntStream.range(0, a).forEach(s -> stringBuilder.append("*"));
+        IntStream.range(0, b).forEach(s -> System.out.println(stringBuilder));
     }
 }
