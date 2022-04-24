34a2242894fe5a9a21da5d31586aa1a4e3df4836
pangnem <sprnd645@gmail.com>
2022-04-24 22:48:32 +0900

    20220424

---
+++ 20220424/README.md
@@ -0,0 +1,5 @@
+# 20220424
+Programmers 직사각형 별찍기 
+
+### 문제 링크
+- https://programmers.co.kr/learn/courses/30/lessons/12969?language=java
---
+++ 20220424/src/main/java/Solution.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        IntStream.range(0, b)
+                .forEach(i -> {
+                    IntStream.range(0, a)
+                            .forEach(j -> System.out.print("*"));
+
+                    System.out.println();
+                });
+    }
+}
