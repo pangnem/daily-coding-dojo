28d3857d849de30960b6593b65eed4af159551bd
pangnem <sprnd645@gmail.com>
2022-04-28 23:54:13 +0900

    20220428 input2 char로 변경

--- 20220428/src/main/java/Main.java
+++ 20220428/src/main/java/Main.java
@@ -1,20 +1,22 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    public int solution(String input1, String input2) {
-        String input1ToLowerCase = input1.toLowerCase();
-
-        return (int) Arrays.stream(input1ToLowerCase.split(""))
-                .filter(input2.toLowerCase()::equals)
-                .count();
-    }
-
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String input1 = in.nextLine();
-        String input2 = in.nextLine();
+        String input1 = in.next();
+        char input2 = in.next().charAt(0);
 
         System.out.println(new Main().solution(input1, input2));
+    }
+
+    public int solution(String input1, char input2) {
+        int count = 0;
+        for (char input1Char : input1.toLowerCase().toCharArray()) {
+            if (input1Char == Character.toLowerCase(input2)) {
+                count++;
+            }
+        }
+
+        return count;
     }
 }
