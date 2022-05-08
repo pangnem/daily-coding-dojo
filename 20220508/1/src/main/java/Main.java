3c3415a0bdc33fe30a9c4130b7ec834fde7b4ca8
pangnem <sprnd645@gmail.com>
2022-05-08 23:49:20 +0900

    20220508 유효한 팰린드롬

--- 20220508/1/src/main/java/Main.java
+++ 20220508/1/src/main/java/Main.java
@@ -9,20 +9,9 @@ public class Main {
     }
 
     public String solution(String input1) {
-        char[] chars = input1.toCharArray();
-        for (int i = 0; i < chars.length / 2; i++) {
-            if (Character.toLowerCase(chars[i]) != Character.toLowerCase(chars[chars.length - 1 - i])) {
-                return "NO";
-            }
-        }
-
-        return "YES";
-    }
-
-    public String solution2(String input1) {
-        String reversedString = new StringBuilder(input1).reverse().toString();
-
-        if (input1.equalsIgnoreCase(reversedString)) {
+        input1 = input1.replaceAll("[^a-zA-Z]", "");
+        String tmp = new StringBuilder(input1).reverse().toString();
+        if (input1.equalsIgnoreCase(tmp)) {
             return "YES";
         }
 
--- 20220508/1/src/test/java/MainTest.java
+++ 20220508/1/src/test/java/MainTest.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.Objects;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class MainTest {
@@ -8,7 +10,27 @@ class MainTest {
     void testCase1() {
         Main main = new Main();
 
-        assertThat(main.solution("gooG")).isEqualTo("YES");
-        assertThat(main.solution2("gooG")).isEqualTo("YES");
+        assertThat(main.solution("found7, time: study; Yduts; emit, 7Dnuof")).isEqualTo("YES");
+    }
+
+    @Test
+    void replaceAll() {
+        String word = "ab;c,d'e";
+
+        assertThat(word.replaceAll("[^[a-zA-Z]]", "")).isEqualTo("abcde");
+    }
+
+    @Test
+    void subString() {
+        String word = "12321";
+
+        assertThat(word.substring(0, word.length() / 2)).isEqualTo("12");
+        assertThat(word.substring(word.length() / 2 + 1)).isEqualTo("21");
+    }
+
+    @Test
+    void objectsEquals() {
+        assertThat(Objects.equals("123", "123")).isTrue();
+        assertThat(Objects.equals("123", "321")).isFalse();
     }
 }
